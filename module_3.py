fd2d86b57511a41553fde0eb304e9103c77cc52f
matthew34w <143020276+matthew34w@users.noreply.github.com>
2023-11-11 14:21:14 -0800

    Add files via upload

---
+++ module_3.py
@@ -0,0 +1,13 @@
+import math
+
+def square_footage(length, width):
+    return int(length) * int(width)
+
+
+
+def calculate_circumference(radius):
+   
+   circumference = 2 * math.pi * int(radius)
+   return circumference
+   
+   
